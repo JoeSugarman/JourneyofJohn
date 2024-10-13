b7f60d27b9c2cf2fff12b91160a1e5e317f1acef
JoeSugarman <62321791+JoeSugarman@users.noreply.github.com>
2024-10-13 20:08:59 +0800

    complete flying bird enemy prefeb

---
+++ Assets/Scripts/Enemy/flyingBird.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class flyingBird : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity; //make sure that the bird can attack only after the cooldown time has passed
+
+    private Health playerHealth;
+
+    private Animator anim;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        if (PlayerInsight())
+        {
+
+            if (cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+                anim.SetTrigger("flying");
+            }
+        }
+
+
+    }
+
+
+    private bool PlayerInsight()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x, boxCollider.bounds.size, 0, Vector2.left, 0, playerLayer);
+        
+        if(hit.collider != null)
+        {
+            playerHealth = hit.transform.GetComponent<Health>();
+            
+        }
+
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x, boxCollider.bounds.size);
+    }
+
+    private void DamagePlayer()
+    {
+        if (PlayerInsight())
+        {
+            playerHealth.TakeDamage(damage);
+        }
+
+    }
+}
