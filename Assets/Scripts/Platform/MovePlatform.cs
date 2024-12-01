c2e594d1781c678900b047a3562beac18d5ba7b1
JoeSugarman <62321791+JoeSugarman@users.noreply.github.com>
2024-12-01 17:43:05 +0800

    updated sprite for mergebox and crusher, modified moving platform script

--- Assets/Scripts/Platform/MovePlatform.cs
+++ Assets/Scripts/Platform/MovePlatform.cs
@@ -8,18 +8,30 @@ public class MovePlatform : MonoBehaviour
     [SerializeField] private Transform pointB;
     [SerializeField] private Transform platform;
     [SerializeField] private float speed = 2f;
+    [SerializeField] private float startDelay = 0f;
 
     private Vector3 nextPosition;
+    private bool canMove = false;
 
     // Start is called before the first frame update
     void Start()
     {
         nextPosition = pointB.position;
+        StartCoroutine(StartDelay());
     }
+
+    private IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(startDelay);
+        canMove = true;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        if (!canMove) return;
+
         transform.position = Vector3.MoveTowards(transform.position, nextPosition, speed * Time.deltaTime);
 
         if(transform.position == pointA.position)
